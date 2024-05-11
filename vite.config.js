cdeb35be5c0bf72eb44871b5fc4a74114e8d7b7c
Fernando <fernandoasm1@gmail.com>
2024-05-11 14:59:38 -0700

    changed-base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Fernandosa21.github.io/MexInvoiceToCSV"
+  base: "/MexInvoiceToCSV/"
 })
